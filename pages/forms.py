67b5169d0f1fb40aacd7d1fa1e8b1de27535a45a
Nabila <nabila.abraham@gmail.com>
2021-01-31 13:53:16 -0500

    working locally, dummy price function

--- pages/forms.py
+++ pages/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class PagesForm(forms.Form):
     item_name = forms.CharField(label = 'Item Name', max_length = 120)
-    zip_code = forms.DecimalField(label = 'Zip Code')
+    zip_code = forms.CharField(label = 'Postal Code', max_length = 6)
--- pages/views.py
+++ pages/views.py
@@ -13,21 +13,10 @@ from os.path import abspath, dirname, join
 
 def get_price(item, zip_code):
     
-    output = subprocess.check_output([python_path, 'webpage_backend_use.py', item, zip_code], \
-                shell=False).decode('utf-8')
-    print('='*30)
-    print(len(output))
-    output1 = output.split(', ')
-    merchant = output1[0].strip("(")
-    price = output1[1].strip(" '' ")
-    url1 = output1[2].replace(")","")
-    url2 = url1.strip()
-    url = url2. strip(" '' ")
-    '''
     merchant='walmart'
     price='12.99'
     url='https://www.nescafe.com/sites/default/files/styles/product_recommendation_large/public/2020-04/NESCAF%C3%89%20Classic_0.png?itok=xEQ6xzPZ'
-    '''
+
     return merchant,price,url
 
 # Create your views here.
